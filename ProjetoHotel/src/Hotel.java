3af37c5409e0cbd1c9e4a197f75c2c148ee997b7
Henrique Maçaneiro <hcmacaneiro@gmail.com>
2023-05-24 17:21:23 -0300

    Criação classes inciais

---
+++ ProjetoHotel/src/Hotel.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+import java.util.ArrayList;
+
+public class Hotel {
+    private String nome;
+    private ArrayList<Quarto> quartos;
+
+    public Hotel(String nome) {
+        this.nome = nome;
+        this.quartos = new ArrayList<>();
+    }
+
+    public void adicionarQuarto(Quarto quarto) {
+        this.quartos.add(quarto);
+    }
+}
---
+++ ProjetoHotel/src/Quarto.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class Quarto {
+    private int numero;
+    private ArrayList<Cliente> hospedes;
+
+    public Quarto(int numero) {
+        this.numero = numero;
+        this.hospedes = new ArrayList<>();
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void adicionarHospede(Cliente cliente) {
+        this.hospedes.add(cliente);
+    }
+}
