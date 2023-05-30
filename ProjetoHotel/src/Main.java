6986ccbb3cf6c469638a25fca1057aed0c3fc128
Henrique Maçaneiro <hcmacaneiro@gmail.com>
2023-05-30 18:52:25 -0300

    Update Main.java

--- ProjetoHotel/src/Main.java
+++ ProjetoHotel/src/Main.java
@@ -13,16 +13,20 @@ public class Main {
             hotel_pjbl.adicionarQuarto(quarto102);
 
             // Criando funcionários
-            Funcionario funcionario1 = new Funcionario("João", "123456789", 5000.0, "01/01/1980", "Rua 123, 456", "11223344", false);
-            Funcionario funcionario2 = new Funcionario("Maria", "987654321", 6000.0, "01/01/1970", "Rua 456, 123", "99887766", true);
+            Funcionario funcionario1 = new Funcionario("João", "123456789", 5000.0,
+                    "01/01/1980", "Rua 123, 456", "11223344", false);
+            Funcionario funcionario2 = new Funcionario("Maria", "987654321", 6000.0,
+                    "01/01/1970", "Rua 456, 123", "99887766", true);
 
             // Adicionando funcionários ao hotel
             hotel_pjbl.adicionarFuncionario(funcionario1);
             hotel_pjbl.adicionarFuncionario(funcionario2);
 
             // Criando clientes
-            Cliente cliente1 = new Cliente("Carlos", "234567890", "carlos@example.com", "Rua 789, 012", "22334455", "01/01/1990", true);
-            Cliente cliente2 = new Cliente("Ana", "876543210", "ana@example.com", "Rua 012, 789", "66778899", "01/01/1985", false);
+            Cliente cliente1 = new Cliente("Carlos", "234567890", "carlos@example.com",
+                    "Rua 789, 012", "22334455", "01/01/1990", true);
+            Cliente cliente2 = new Cliente("Ana", "876543210", "ana@example.com",
+                    "Rua 012, 789", "66778899", "01/01/1985", false);
 
             // Adicionando clientes ao quarto 101
             quarto101.adicionarHospede(cliente1);
