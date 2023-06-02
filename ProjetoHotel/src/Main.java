84f4b6812f297a52dc621d24060922bb852a3adc
DiegoASCW <122821160+DiegoASCW@users.noreply.github.com>
2023-06-02 14:32:59 -0300

    Manipulação de objeto

--- ProjetoHotel/src/Main.java
+++ ProjetoHotel/src/Main.java
@@ -1,68 +1,63 @@
 public class Main {
     public static void main(String[] args) {
-        try {
-            // Criando um hotel
-            Hotel hotel_pjbl = new Hotel("Hotel PJBL");
+        String fileName = "caminho/do/arquivo/file.txt";
+        Hotel hotel = null;
 
-            // Criando quartos
-            Quarto quarto101 = new Quarto(101);
-            Quarto quarto102 = new Quarto(102);
+        // Checa se o arquivo Existe
+        boolean fileExists = new File(fileName).exists();
 
-            // Adicionando quartos ao hotel
-            hotel_pjbl.adicionarQuarto(quarto101);
-            hotel_pjbl.adicionarQuarto(quarto102);
+        // Lê o arquivo
+        if (fileExists) {
+            try (ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream(fileName))) {
+                hotel = (Hotel) inputStream.readObject();
+            } catch (IOException | ClassNotFoundException e) {
+                System.out.println("Ocorreu um erro na leitura do arquivo: " + e.getMessage());
+            }
+        }
 
-            // Criando funcionários
-            Funcionario funcionario1 = new Funcionario("João", "123456789", 5000.0,
-                    "01/01/1980", "Rua 123, 456", "11223344", false);
-            Funcionario funcionario2 = new Funcionario("Maria", "987654321", 6000.0,
-                    "01/01/1970", "Rua 456, 123", "99887766", true);
+        // Caso o 'hotel' não exista, cria um novo
+        if (!fileExists || hotel == null) {
+            String nome;
+            Scanner scanner = new Scanner(System.in);
 
-            // Adicionando funcionários ao hotel
-            hotel_pjbl.adicionarFuncionario(funcionario1);
-            hotel_pjbl.adicionarFuncionario(funcionario2);
+            System.out.println("Nenhum objeto de 'hotel' encontrado, crie um...");
+            System.out.println("Qual o nome do Hotel:");
+            nome = scanner.nextLine();
 
-            // Criando clientes
-            Cliente cliente1 = new Cliente("Carlos", "234567890", "carlos@example.com",
-                    "Rua 789, 012", "22334455", "01/01/1990", true);
-            Cliente cliente2 = new Cliente("Ana", "876543210", "ana@example.com",
-                    "Rua 012, 789", "66778899", "01/01/1985", false);
+            // Cria o Obj Hotel
+            try {
+                hotel = new Hotel(nome);
+                System.out.println("Novo Hotel criado!");
 
-            // Adicionando clientes ao quarto 101
-            quarto101.adicionarHospede(cliente1);
-            quarto101.adicionarHospede(cliente2);
+                // Obj Quarto
+                Quarto quarto101 = new Quarto(101);
+                Quarto quarto102 = new Quarto(102);
+                hotel.adicionarQuarto(quarto101);
+                hotel.adicionarQuarto(quarto102);
 
-            // Mostrando informações do hotel
-            hotel_pjbl.mostrar();
+                // Obj Funcionario
+                Funcionario funcionario1 = new Funcionario("João", "123456789", 5000.0, "01/01/1980", "Rua 123, 456", "11223344", false);
+                Funcionario funcionario2 = new Funcionario("Maria", "987654321", 6000.0, "01/01/1970", "Rua 456, 123", "99887766", true);
+                hotel.adicionarFuncionario(funcionario1);
+                hotel.adicionarFuncionario(funcionario2);
 
-            // Mostrando informações de cada funcionário
-            funcionario1.mostrar();
-            funcionario1.status();
-            System.out.println("Salário do Funcionario 1: " + funcionario1.getSalario());
+                // Obj Cliente
+                Cliente cliente1 = new Cliente("Carlos", "234567890", "carlos@example.com", "Rua 789, 012", "22334455", "01/01/1990", true);
+                Cliente cliente2 = new Cliente("Ana", "876543210", "ana@example.com", "Rua 012, 789", "66778899", "01/01/1985", false);
+                quarto101.adicionarHospede(cliente1);
+                quarto101.adicionarHospede(cliente2);
+            } catch (HotelException e) {
+                System.out.println("An error occurred while creating the hotel object: " + e.getMessage());
+                return;
+            }
 
-            funcionario2.mostrar();
-            funcionario2.status();
-            System.out.println("Salário do Funcionario 2: " + funcionario2.getSalario());
-
-            // Alterando o nome do funcionario1
-            funcionario1.setNome("João Silva");
-            System.out.println("Novo nome do Funcionario 1: " + funcionario1.getNome());
-
-            // Mostrando informações de cada cliente
-            cliente1.mostrar();
-            cliente1.status();
-            System.out.println("Email do Cliente 1: " + cliente1.getEmail());
-
-            cliente2.mostrar();
-            cliente2.status();
-            System.out.println("Email do Cliente 2: " + cliente2.getEmail());
-
-            // Mostrando o número dos quartos
-            System.out.println("Número do Quarto 101: " + quarto101.getNumero());
-            System.out.println("Número do Quarto 102: " + quarto102.getNumero());
-
-        } catch (HotelException e) {
-            e.printStackTrace();
+            // Salva o objeto hotel no arquivo .txt
+            try (ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream(fileName))) {
+                outputStream.writeObject(hotel);
+                System.out.println("O Hotel foi salvo.");
+            } catch (IOException e) {
+                System.out.println("Um erro ocorreu ao salvar o Hotel: " + e.getMessage());
+            }
         }
     }
 }
